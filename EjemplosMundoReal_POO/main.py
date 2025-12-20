641bdb3a4843d80fd2343838cb95f293ea9257c8
Palexander10 <pa.ramonm@uea.edu.ec>
2025-12-20 18:22:31 -0500

    Cracion de la funcion principal

---
+++ EjemplosMundoReal_POO/main.py
@@ -0,0 +1,29 @@
+# Archivo: main.py
+# Programa principal
+# Descripción:
+# Este archivo ejecuta el sistema de voletos, creando los
+# objetos necesarios y demostrando la interacción entre ellos.
+
+from avion import Avion
+from asiento import Asiento
+
+# Creación del objeto Avion
+mi_avion = Avion("Boeing 747")
+
+# Creación de objetos Asiento
+asiento1 = Asiento("1A", "Ejecutiva")
+asiento2 = Asiento("15C", "Turista")
+
+# Asociación de los asientos al avión
+mi_avion.agregar_asiento(asiento1)
+mi_avion.agregar_asiento(asiento2)
+
+# Muestra el estado inicial
+mi_avion.mostrar_estado_vuelo()
+
+# Reserva de un asiento
+print("\n--- Realizando reserva ---")
+asiento1.reservar()
+
+# Muestra el estado final tras la reserva
+mi_avion.mostrar_estado_vuelo()
